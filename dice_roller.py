5195fc36b7fcbfa41045da8c1d8a3b75533845c6
joonakor-uef <63606051+joonakor-uef@users.noreply.github.com>
2020-05-03 17:38:41 +0300

    Cleaned up, added empty line to print.

--- dice_roller.py
+++ dice_roller.py
@@ -1,13 +1,19 @@
-def main():
-  import random
-  dice_rolls = 2;
+import random
 
+def main():
+  dice_rolls = 2;
   dice_sum = 0
+
   for i in range (0,dice_rolls):
     roll = random.randint(1,6)
-    dice_sum += roll 
-    print(f'You rolled a {roll}.')
-  print(f'You rolled a total of {dice_sum}.')
-
+    dice_sum += roll
+    if roll == 1:
+      print(f'You rolled a {roll}. Critical failure.')
+    elif roll == 6:
+      print (f'You rolled a {roll}. Success.')
+    else:
+      print(f'You rolled a {roll}.')  
+  print(f'\nYou rolled a total of {dice_sum}.')
+ 
 if __name__== "__main__":
   main()
